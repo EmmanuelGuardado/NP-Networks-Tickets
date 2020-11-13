a1515d46fe936b625e44c3d567f8d8af99202780
EmmanuelGuardado <emmanuelgangel@gmail.com>
2020-11-13 02:31:31 -0600

    Agregando funcionalidad de cambio de contraseña

---
+++ Web/Areas/Identity/ViewsModels/CambioContrasenaViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Areas.Identity.ViewsModels
+{
+    public class CambioContrasenaViewModel
+    {
+    }
+}
